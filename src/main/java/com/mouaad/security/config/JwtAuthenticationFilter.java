7ba1cdeb77406a3a956535184f6920d5d2dd4fc1
Mouaad Elkhadari <mouaad.elkhadari@gmail.com>
2024-08-03 20:46:18 +0100

    apres la creation de getAllClaims(String token)

--- src/main/java/com/mouaad/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/mouaad/security/config/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
--- src/main/java/com/mouaad/security/config/JwtService.java
+++ src/main/java/com/mouaad/security/config/JwtService.java
@@ -1,7 +1,21 @@
 package com.mouaad.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JwtService {
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims getAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
