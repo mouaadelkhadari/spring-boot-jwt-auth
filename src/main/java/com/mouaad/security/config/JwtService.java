ec38be3af1b00924c7d33eacdb54e0a1b2754589
Mouaad Elkhadari <mouaad.elkhadari@gmail.com>
2024-08-03 21:11:33 +0100

    apres ajouter la cle secrete

--- src/main/java/com/mouaad/security/config/JwtService.java
+++ src/main/java/com/mouaad/security/config/JwtService.java
@@ -2,10 +2,15 @@ package com.mouaad.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "3cc494ddb0c042ee06b2ff08dfbe6c85ec48be48afe57a6652a2729c29dd6883";
     public String extractUsername(String token) {
         return null;
     }
@@ -17,5 +22,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
