415dea7ff2f861dd2ac0686c5c8ce62a78b0a940
Mouaad Elkhadari <mouaad.elkhadari@gmail.com>
2024-08-04 13:53:44 +0100

    apres la verification de token

--- src/main/java/com/mouaad/security/config/JwtService.java
+++ src/main/java/com/mouaad/security/config/JwtService.java
@@ -43,6 +43,19 @@ public class JwtService {
                 .compact();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpirtion(token).before(new Date());
+    }
+
+    private Date extractExpirtion(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
